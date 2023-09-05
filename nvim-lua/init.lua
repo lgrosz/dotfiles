3b9c6d90e4bfed74ebabdf647fa4d54b30f6741c
Logan Grosz <logan.grosz@gmail.com>
2023-09-04 21:56:40 -0600

    Cleanup and some plugin additions
    - Moves tree sitter config to treesitter.lua
    - Moves fzf-lua config to fzf-lua.lua
    - Adds markdown plugins
        - Adds vim-markdown
        - Adds markdown-preview
    - Adds fd to exit insert mode

--- nvim-lua/init.lua
+++ nvim-lua/init.lua
@@ -20,76 +20,20 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Global helper functions
+function map(mode, lhs, rhs, opts)
+  local options = { noremap=true, silent=true }
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
 -- Install plugins
 require('lazy').setup({
 	{ import = 'custom.plugins' },
 })
 
--- TODO How do I get this to go somewhere else?
--- [[ Configure Treesitter ]]
--- See `:help nvim-treesitter`
-require('nvim-treesitter.configs').setup {
-  -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'bitbake' },
-
-  -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-  auto_install = false,
-
-  highlight = { enable = true },
-  indent = { enable = true },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = '<c-space>',
-      node_incremental = '<c-space>',
-      scope_incremental = '<c-s>',
-      node_decremental = '<M-space>',
-    },
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ['aa'] = '@parameter.outer',
-        ['ia'] = '@parameter.inner',
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
-      },
-      goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
-      },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
-      },
-      goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
-      },
-    },
-    swap = {
-      enable = true,
-      swap_next = {
-        ['<leader>a'] = '@parameter.inner',
-      },
-      swap_previous = {
-        ['<leader>A'] = '@parameter.inner',
-      },
-    },
-  },
-}
+map('i', 'fd', '<Esc>')
 
 vim.api.nvim_create_user_command('Scratch', require'tools'.makeScratch, {})
--- nvim-lua/lua/custom/plugins/fzf-lua.lua
+++ nvim-lua/lua/custom/plugins/fzf-lua.lua
@@ -4,7 +4,12 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     -- calling `setup` is optional for customization
-    require("fzf-lua").setup({})
-  end
+    require("fzf-lua").setup({ })
+
+    -- keymaps
+    map("n", "ff", "<Cmd>Fzf files<Cr>", { silent = true, desc = "Find files" })
+    map("n", "fg", "<Cmd>Fzf grep<Cr>", { silent = true, desc = "Grep in files" })
+    map("n", "fgg", "<Cmd>Fzf grep<Cr><Cr>", { silent = true, desc = "Grep if files, without prompt" })
+  end,
 }
 
