19b8c15281a4a95c03b3b314e22ea0d090681161
Logan Grosz <logan.grosz@gmail.com>
2023-11-12 22:30:10 -0700

    Replace noice with fidget

--- nvim-lua/lua/custom/plugins/lspconfig.lua
+++ nvim-lua/lua/custom/plugins/lspconfig.lua
@@ -89,10 +89,6 @@ return {
       end,
     },
 
-    -- Useful status updates for LSP
-    -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-    { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
-
     -- Additional lua configuration, makes nvim stuff amazing!
     {
       'folke/neodev.nvim',
