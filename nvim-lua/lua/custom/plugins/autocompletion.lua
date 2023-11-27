602e32303f5eeb2a94df9e0e733b13e616d77ad3
Logan Grosz <logan.grosz@gmail.com>
2023-11-26 23:10:03 -0700

    Adds LuaSnip

--- nvim-lua/lua/custom/plugins/autocompletion.lua
+++ nvim-lua/lua/custom/plugins/autocompletion.lua
@@ -12,6 +12,10 @@ return {
     -- LSP
     'hrsh7th/cmp-nvim-lsp',
 
+    -- Snippets
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
+
     -- Misc
     'quangnguyen30192/cmp-nvim-tags',
     'petertriho/cmp-git',
@@ -19,6 +23,11 @@ return {
   config = function ()
     local cmp = require('cmp')
     cmp.setup {
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      },
       mapping = cmp.mapping.preset.insert {
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
