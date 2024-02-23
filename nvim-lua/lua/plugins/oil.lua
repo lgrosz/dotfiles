28575275464827657c622b12b8a58bd0d07ca8fa
Logan Grosz <logan.grosz@b9c.com>
2024-02-23 09:14:02 -0700

    Adds [N]avigate [F]iles

--- nvim-lua/lua/plugins/oil.lua
+++ nvim-lua/lua/plugins/oil.lua
@@ -1,6 +1,12 @@
 return {
   'stevearc/oil.nvim',
   config = function ()
-    require("oil").setup()
+    local oil = require("oil")
+
+    -- TODO Configure float to look like fzf-lua
+    oil.setup({ })
+
+    -- keymaps
+    vim.keymap.set('n', '<leader>nf', oil.toggle_float, { desc = '[N]avigate [F]iles' })
   end
 }
