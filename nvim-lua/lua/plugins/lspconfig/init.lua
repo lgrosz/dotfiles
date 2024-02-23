38e44425a178d1bacb0cfb6f49c0d2cff6f4fdd0
Logan Grosz <logan.grosz@b9c.com>
2024-02-23 08:57:22 -0700

    Adds cmake language server

--- nvim-lua/lua/plugins/lspconfig/init.lua
+++ nvim-lua/lua/plugins/lspconfig/init.lua
@@ -3,6 +3,7 @@ return {
   config = function()
     require("plugins.lspconfig.clangd")
     require("plugins.lspconfig.lua-language-server")
+    require("plugins.lspconfig.cmake-language-server")
   end,
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
