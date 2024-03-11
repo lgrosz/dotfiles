74e7957fede80c5bf5d65950df7e6c107aca1be0
Logan Grosz <logan.grosz@gmail.com>
2024-03-10 21:43:57 -0600

    Adds accept-match on CR only when hovered
    Allows for more logical snippet acceptance

--- nvim-lua/.config/nvim/lua/plugins/cmp/mapping.lua
+++ nvim-lua/.config/nvim/lua/plugins/cmp/mapping.lua
@@ -32,6 +32,15 @@ mapping.preset = {
       fallback()
     end
   end, { "i", "c", "s" }),
+  -- Accept match, only if hovered
+  ["<CR>"] = cmp.mapping(function(fallback)
+    if cmp.visible() and cmp.get_active_entry() then
+      cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+    else
+      fallback()
+    end
+  end
+  ),
   -- End completion
   ["<C-e>"] = cmp.mapping(function(fallback)
     if cmp.visible() then
