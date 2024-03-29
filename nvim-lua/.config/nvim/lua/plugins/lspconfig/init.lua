bd1b8db2455ae10007ee61640bc8f4c68369cdba
Logan Grosz <logan.grosz@gmail.com>
2024-03-29 00:02:12 -0600

    Adds cmake-language-server configuration

--- nvim-lua/.config/nvim/lua/plugins/lspconfig/init.lua
+++ nvim-lua/.config/nvim/lua/plugins/lspconfig/init.lua
@@ -3,6 +3,7 @@ return {
   config = function()
     require("plugins.lspconfig.clangd")
     require("plugins.lspconfig.lua-language-server")
+    require("plugins.lspconfig.cmake-language-server")
   end,
   dependencies = {
     "folke/neodev.nvim",
