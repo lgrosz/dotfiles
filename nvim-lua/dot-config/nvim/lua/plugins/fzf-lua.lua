d50b4b1b8b237ead581947078def428fea947caf
Logan Grosz <logan.grosz@b9c.com>
2024-05-06 14:16:57 -0600

    feat(keymaps): add command and history keymaps

--- nvim-lua/dot-config/nvim/lua/plugins/fzf-lua.lua
+++ nvim-lua/dot-config/nvim/lua/plugins/fzf-lua.lua
@@ -12,6 +12,9 @@ return {
     vim.keymap.set('n', '<leader>sb', fzf_lua.buffers, { desc = '[S]earch [B]uffers' })
     vim.keymap.set('n', '<leader>sg', fzf_lua.grep_project, { desc = '[S]earch with [G]rep' })
     vim.keymap.set('n', '<leader>sw', fzf_lua.grep_cword, { desc = '[S]earch current [W]ord' })
+    vim.keymap.set('n', '<leader>s:', fzf_lua.commands, { desc = '[S]earch commands' })
+    vim.keymap.set('n', '<leader>sh/', fzf_lua.command_history, { desc = '[S]earch [H]istory of commands' })
+    vim.keymap.set('n', '<leader>sh:', fzf_lua.search_history, { desc = '[S]earch [H]istory of searches' })
 
     vim.keymap.set("n", "<leader>gf", fzf_lua.git_files, { silent = true, desc = 'Search [G]it [F]iles' })
   end,
