551a93306e13e34e2fa35ca4b0e6c7195ca450e6
Logan Grosz <logan.grosz@b9c.com>
2024-11-13 14:13:34 -0700

    feat(lsp): add pyright configuration

--- nvim-lua/dot-config/nvim/lua/plugins/lspconfig/init.lua
+++ nvim-lua/dot-config/nvim/lua/plugins/lspconfig/init.lua
@@ -6,6 +6,7 @@ return {
     require("plugins.lspconfig.cmake-language-server")
     require("plugins.lspconfig.dockerfile-language-server")
     require("plugins.lspconfig.mesonlsp")
+    require("plugins.lspconfig.pyright")
   end,
   dependencies = {
     "folke/neodev.nvim",
