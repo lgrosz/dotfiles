def8f4ad6bd655c4b218fe731fded0c9b7f23773
Logan Grosz <logan.grosz@gmail.com>
2026-04-01 14:08:45 -0600

    fix(statusline): reference correct theme name

--- nvim-lua/dot-config/nvim/lua/plugins/statusline.lua
+++ nvim-lua/dot-config/nvim/lua/plugins/statusline.lua
@@ -5,7 +5,7 @@ return {
   opts = {
     options = {
       icons_enabled = false,
-      theme = "catppuccin",
+      theme = "catppuccin-frappe",
       component_separators = '|',
       section_separators = '',
     },
