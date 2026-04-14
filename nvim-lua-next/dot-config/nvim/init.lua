7395dc91da2c72881ef7a20892dcee2bcdec00bb
Logan Grosz <logan.grosz@gmail.com>
2026-04-13 23:58:27 -0600

    fzf-lua(nvim-lua-next): load on demand in keymaps

--- nvim-lua-next/dot-config/nvim/init.lua
+++ nvim-lua-next/dot-config/nvim/init.lua
@@ -12,12 +12,12 @@ fzf_lua.setup({
   fzf_colors = true,
 })
 
-vim.keymap.set('n', '<leader>sf', fzf_lua.files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sb', fzf_lua.buffers, { desc = '[S]earch [B]uffers' })
-vim.keymap.set('n', '<leader>sg', fzf_lua.grep_project, { desc = '[S]earch with [G]rep' })
-vim.keymap.set('n', '<leader>sw', fzf_lua.grep_cword, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>s:', fzf_lua.commands, { desc = '[S]earch commands' })
-vim.keymap.set('n', '<leader>sh:', fzf_lua.command_history, { desc = '[S]earch [H]istory of commands' })
-vim.keymap.set('n', '<leader>sh/', fzf_lua.search_history, { desc = '[S]earch [H]istory of searches' })
-vim.keymap.set("n", "<leader>gf", fzf_lua.git_files, { desc = 'Search [G]it [F]iles' })
+vim.keymap.set('n', '<leader>sf', '<Cmd>lua require("fzf-lua").files()<CR>', { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sb', '<Cmd>lua require("fzf-lua").buffers()<CR>', { desc = '[S]earch [B]uffers' })
+vim.keymap.set('n', '<leader>sg', '<Cmd>lua require("fzf-lua").grep_project()<CR>', { desc = '[S]earch with [G]rep' })
+vim.keymap.set('n', '<leader>sw', '<Cmd>lua require("fzf-lua").grep_cword()<CR>', { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>s:', '<Cmd>lua require("fzf-lua").commands()<CR>', { desc = '[S]earch commands' })
+vim.keymap.set('n', '<leader>sh:', '<Cmd>lua require("fzf-lua").command_history()<CR>', { desc = '[S]earch [H]istory of commands' })
+vim.keymap.set('n', '<leader>sh/', '<Cmd>lua require("fzf-lua").search_history()<CR>', { desc = '[S]earch [H]istory of searches' })
+vim.keymap.set("n", "<leader>gf", '<Cmd>lua require("fzf-lua").git_files()<CR>', { desc = 'Search [G]it [F]iles' })
 
