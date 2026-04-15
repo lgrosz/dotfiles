e12551fcfab0449839f115b61adb7f08adaeeb0d
Logan Grosz <logan.grosz@gmail.com>
2026-04-14 18:46:22 -0600

    plugins(nvim-lua-next): add my favorite tpope plugins

--- nvim-lua-next/dot-config/nvim/init.lua
+++ nvim-lua-next/dot-config/nvim/init.lua
@@ -2,6 +2,8 @@
 -- plugin/30-plugins.lua
 vim.pack.add({
   'https://github.com/ibhagwan/fzf-lua',
+  'https://github.com/tpope/vim-abolish',
   'https://github.com/tpope/vim-fugitive',
+  'https://github.com/tpope/vim-surround',
 })
 
