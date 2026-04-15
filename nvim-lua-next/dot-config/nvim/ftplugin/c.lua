702ee96db453d143d494e5b1958287e1065f0683
Logan Grosz <logan.grosz@gmail.com>
2026-04-14 21:39:20 -0600

    wip! c(nvim-lua-next): add ftplugin/c.lua
    WIP: Not sure how much I like how I'm installing stuff in my ftplugin,
      but where else if I don't want it unless I open a c-file. Also, what
      if I want to open a c-file and have nvim-treesitter indent working
      without reloading the buffer?
    
      I think I want to set up some language installation logic in
      30-plugins.lua as it is specific to nvim-lspconfig, then my ftplugin
      can just assume that that is the case and require nvim-lspconfig..
    
      Finally, should this be in after/?

---
+++ nvim-lua-next/dot-config/nvim/ftplugin/c.lua
@@ -0,0 +1,16 @@
+require('nvim-treesitter').install('c')
+
+if vim.treesitter.language.add('c') then
+  vim.treesitter.start()
+
+  -- use neovim treesitter folding
+  vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+  vim.wo.foldmethod = 'expr'
+
+  -- nvim-treesitter provides indent queries
+  if vim.tbl_contains(require('nvim-treesitter').get_installed('queries'), 'c') then
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  else
+    vim.bo.cindent = true
+  end
+end
