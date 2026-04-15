0b8d634e37c14093e0353f67733f8e51b1ecf709
Logan Grosz <logan.grosz@gmail.com>
2026-04-14 18:33:20 -0600

    lsp(nvim-lua): remove unused keymaps
    I've never used these.

--- nvim-lua/dot-config/nvim/init.lua
+++ nvim-lua/dot-config/nvim/init.lua
@@ -45,11 +45,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = { buffer = ev.buf }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
   end,
 })
 
