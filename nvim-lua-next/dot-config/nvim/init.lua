006c480e1bce5704fb521fe13bb34a35f6b12d49
Logan Grosz <logan.grosz@gmail.com>
2026-04-14 21:41:40 -0600

    init(nvim-lua-next): update parsers when nvim-treesitter updates

--- nvim-lua-next/dot-config/nvim/init.lua
+++ nvim-lua-next/dot-config/nvim/init.lua
@@ -1,3 +1,12 @@
+-- update nvim-treesitter parsers
+vim.api.nvim_create_autocmd('PackChanged', { callback = function(ev)
+  local name, kind = ev.data.spec.name, ev.data.kind
+  if name == 'nvim-treesitter' and kind == 'update' then
+    if not ev.data.active then vim.cmd.packadd('nvim-treesitter') end
+    vim.cmd('TSUpdate')
+  end
+end })
+
 -- Download plugins. If needed, plugins are configured in
 -- plugin/30-plugins.lua
 vim.pack.add({
